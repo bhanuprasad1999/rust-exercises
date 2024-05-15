c0b1e37fa586f4ce7ef8f67b34295e4117ca3943
bhanuprasad1999 <bp3545486@gmail.com>
2024-05-15 11:41:55 +0530

    added println

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
+fn user_input() -> i32{
+    let mut input:String = String::new();
+    let output:i32 ;
+    match io::stdin().read_line(&mut input){
+        Ok(n) => {
+            println!("{n}");
+            output = input.trim().parse::<i32>().unwrap();
+            return output
+        }
+        Err(error)=>{
+            panic!("Expected Integers, but got String!{}", error);
+        }
+
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let choice :i32; 
+    choice = user_input();
+    println!("One month challenge of coding with RUST");
+    println!("selected the {} choice", choice);
+    match choice {
+        1 => {
+            println!("{}", choice);
+        }
+        _ => {
+            print!("{} is not a valid choice", choice);
+        }
+    }
 }
